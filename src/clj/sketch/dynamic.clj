c7404b76bb8697590d17d40d218ea0a612395ed7
Ema Milojkovic <milojkovic.ema@gmail.com>
2018-05-09 00:36:15 -0500

    i can make a gradient now

--- src/clj/sketch/dynamic.clj
+++ src/clj/sketch/dynamic.clj
@@ -1,5 +1,6 @@
 (ns sketch.dynamic
   (:require [quil.core :refer :all])
+  (:require [utils.utils :refer :all])
   (:use [incanter.core :only [$=]])
   (:use [clojure.math.combinatorics :only [combinations cartesian-product]])
   (:use [clojure.pprint])
@@ -16,8 +17,12 @@
   ; brightness in [0, 100], and alpha in [0.0, 1.0] -- instead of RBG
   (color-mode :hsb 360 100 100 1.0)
 
-  (background 220 49 66)
-  (rect 100 100 400 400)
+  ;(background 220 49 66)
+  (doseq [y (range 0 1000 5)]  ; loop y from 0 to 1000 in increments of 5
+    ;(let [hue (rescale y 0 1000 130 220)] ; green at the top, blue at the bottom
+    (let [hue (rescale (random y (+ y 100)) 0 1000 130 220)] ; introduce some randomness
+      (fill hue 90 90)
+      (rect 0 y 1000 5)))  ; draw a rectangle that spans the width of the image
   (save "sketch.tif")
 )
 
